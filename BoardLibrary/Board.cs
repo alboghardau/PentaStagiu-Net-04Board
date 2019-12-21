00fb21897d6fb4d73b1cb46b3896630139533594
albog <alboghardau@gmail.com>
2019-12-21 22:30:41 +0200

    Board singleton class with XML Serialization

--- BoardLibrary/Board.cs
+++ BoardLibrary/Board.cs
@@ -1,35 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//Singleton class for the board data
+//SINGLETON BOARD CLASS TO HANDLE THE DATA
 namespace BoardLibrary
 {
     public class Board
     {
-        private static readonly Board instance = new Board();
-        List<Post> postList = new List<Post>();
-        List<User> userList = new List<User>();
-        User loggedUser;
-
-        static Board()
-        {
-
-        }
+        private static Board instance = null;
+        private List<Post> PostList { get; set; }
+        private List<User> UserList {get; set; }
+        User LoggedUser { get; set; }
 
         private Board()
         {
+            this.PostList = new List<Post>();
+            this.UserList = new List<User>();
 
+            //LOARD BOARD DATA FROM XML
+            try
+            {
+                this.PostList = XmlSerialization.ReadFromXmlFile<List<Post>>("posts.xml");
+                this.UserList = XmlSerialization.ReadFromXmlFile<List<User>>("users.xml");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("###FAILED TO LOAD XML DATA!###");
+            }            
         }
+
         public static Board Instance
         { 
             get
             {
+                if(instance == null)
+                {
+                    instance = new Board();
+                }
                 return instance;
             }
         }
 
+        //SAVE BOARD DATA TO XML
+        public void SaveData()
+        {
+            XmlSerialization.WriteToXmlFile<List<Post>>("posts.xml", instance.PostList);
+            XmlSerialization.WriteToXmlFile<List<User>>("users.xml", instance.UserList);
+        }               
     }
 }
